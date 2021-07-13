d46c7b87d5ae0e2931283c32996163870b60b67f
Gahao <55437359+gahao0168@users.noreply.github.com>
2021-07-13 17:07:18 +0800

    complete
    adddata..py

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from mysite.models import Post
+from mysite.models import Post,Country,City
 
 # Register your models here.
 
@@ -7,3 +7,13 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date')
 
 admin.site.register(Post, PostAdmin)
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country_id')
+
+admin.site.register(Country, CountryAdmin)
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'population', 'country')
+
+admin.site.register(City, CityAdmin)
--- mysite/models.py
+++ mysite/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    pub_date = models.DateTimeField(default=timezone.now)
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+	pub_date = models.DateTimeField(default=timezone.now)
+	class Meta:
+		ordering = ('-pub_date',)
+	def __str__(self):
+		return self.title
 
-    class Meta:
-        ordering = ('-pub_date',)
+class Country(models.Model):
+	country_id = models.IntegerField()
+	name = models.CharField(max_length=50)
+	def __str__(self):
+		return self.name
 
-    def __str__(self):
-        return self.title
+class City(models.Model):
+	name = models.CharField(max_length=50)
+	population = models.IntegerField()
+	country = models.ForeignKey(Country, on_delete=models.CASCADE)
+	def __str__(self):
+		return name
