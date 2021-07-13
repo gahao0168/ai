b988678113f932fbd2afe8dcc505a9a1b153d6a3
Gahao <55437359+gahao0168@users.noreply.github.com>
2021-07-13 10:59:27 +0800

    first commit

---
+++ mysite/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+def index(request):
+	names = "謝昌儒"
+	lotto = [random.randint(1,49) for i in range(6)]
+	special = lotto[0]
+	lotto = lotto[1:6]
+	return render(request, "index.html", locals())
